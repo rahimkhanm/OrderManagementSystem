b3249b3c5e4af0f7de4fa226701075166492f487
rahimkhanm <70720464+rahimkhanm@users.noreply.github.com>
2020-09-04 00:31:08 +0530

    Book order Data access Changes

--- Book.cs
+++ Book.cs
@@ -1,1 +1,22 @@
+//Data Access Layer Implementation 
 
+using System;
+
+namespace PaymentGateWay
+{
+     public class Book :IProductRepository
+     {
+        public string GeneratePackingSlip()
+        {
+          return "Book order packing slip generaed ";
+        }
+        
+       public void GetAgentCommisionDetails()
+       {
+            console.Writeline(" Book order agent commision is 5%");
+       }
+       
+     }
+     
+   
+}

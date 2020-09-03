018c9b54bf054bbbf91293f84b5708caa9c3ed78
rahimkhanm <70720464+rahimkhanm@users.noreply.github.com>
2020-09-04 00:44:38 +0530

    Product Type design changes

--- Program.cs
+++ Program.cs
@@ -8,22 +8,28 @@ namespace OrderManagement
     public class Program
     {
           string paymentOrderType;
-	        string packingSlipDetails;
+	  string packingSlipDetails;
           console.WriteLine(" Please enter product type");
           paymentOrderType=console.ReadLine();
           ProductAbstract objAProduct=new ConcreteProuduct();
           MemberShipAbstract objAMembership= new ConcreteMemberShip();
           
-		  if(paymentOrderType=="Book")
-		  {
-			 IProductRepository objBook=objProduct.GetPaymentDetailsByProductType(paymentOrderType); 
-			 packingSlipDetails=objBook.GeneratePaySlip();
-			 objBook.GetCommisionDetails();
-			 Console.WriteLine(packingSlipDetails);
-		  }
-		 
+	  if(paymentOrderType=="Book")
+	  {
+		 IProductRepository objBook=objProduct.GetPaymentDetailsByProductType(paymentOrderType); 
+		 packingSlipDetails=objBook.GeneratePaySlip();
+		 objBook.GetCommisionDetails();
+		 Console.WriteLine(packingSlipDetails);
+	  }
+	   else if (paymentOrderType=="PhysicalProudct")
+	  {
+
+		  IProductRepository objPhysicalProduct=objProduct.GetPaymentDetailsByProductType(paymentOrderType); 
+		  packingSlipDetails=objPhysicalProduct.GeneratePaySlip();
+		  objPhysicalProduct.GetCommisionDetails();
+	  } 
 		  
-      Console.ReadLine();
+      		Console.ReadLine();
         
     }
 }

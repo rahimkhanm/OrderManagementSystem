b8b656c0290fd80a2c47e1edfadc1d745a8b343b
rahimkhanm <70720464+rahimkhanm@users.noreply.github.com>
2020-09-04 00:40:13 +0530

    Book class design changes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
+//Presentation Layer
+
+﻿using System;
+using System.Collections.Generic;
+
+namespace OrderManagement
+{
+    public class Program
+    {
+          string paymentOrderType;
+	        string packingSlipDetails;
+          console.WriteLine(" Please enter product type");
+          paymentOrderType=console.ReadLine();
+          ProductAbstract objAProduct=new ConcreteProuduct();
+          MemberShipAbstract objAMembership= new ConcreteMemberShip();
+          
+		  if(paymentOrderType=="Book")
+		  {
+			 IProductRepository objBook=objProduct.GetPaymentDetailsByProductType(paymentOrderType); 
+			 packingSlipDetails=objBook.GeneratePaySlip();
+			 objBook.GetCommisionDetails();
+			 Console.WriteLine(packingSlipDetails);
+		  }
+		 
+		  
+      Console.ReadLine();
+        
+    }
+}
 

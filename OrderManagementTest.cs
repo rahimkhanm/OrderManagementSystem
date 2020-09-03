5b35250dcaeaf6e6d8216cecabb6a82c6058da2e
rahimkhanm <70720464+rahimkhanm@users.noreply.github.com>
2020-09-04 00:37:42 +0530

    Book class Unit Test Cases

--- OrderManagementTest.cs
+++ OrderManagementTest.cs
@@ -1,1 +1,23 @@
+using Microsoft.VisuvalStudio.TestingTools.UnitTesting;
+using PayamentGateWay;
+
+namespace UnitTestProject
+{
+[TestClass]
+
+public class UnitTest1
+{
+  [TestMethod]
+  public void GenrateBookOrderPackiSlip()
+  {
+       ProductAbstract objAbstract= new ConcreteProuduct();
+       IPayamentRepository objBook= objAbstract.GetPaymentDetailsByProductType("Book");
+       string expected="Book order packing slip generaed";
+       string actuval=objBook.GeneratePackingSlip();
+       Assert.AreEqual(expected,actuval);
+
+  }
+  
+}
+}
 

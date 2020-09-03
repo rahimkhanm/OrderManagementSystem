f2157906a4c8fc60a5d885df95c78e05da2ef582
rahimkhanm <70720464+rahimkhanm@users.noreply.github.com>
2020-09-04 00:33:39 +0530

    Video class changes

--- Video.cs
+++ Video.cs
@@ -1,1 +1,20 @@
+using System;
 
+namespace PaymentGateWay
+{
+     public class PhysicalProduct :IProductRepository
+     {
+        public string GeneratePackingSlip()
+        {
+          return "Video packing slip generaed ";
+        }
+        
+       public void GetAgentCommisionDetails()
+       {
+            console.Writeline(" Vedio agent commision percentage is 10%");
+       }
+       
+     }
+       
+    
+}
